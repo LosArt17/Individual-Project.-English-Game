fd018ee79174e49f125ba4ae491744a857175915
LosArt17 <my.odna.comanda@gmail.com>
2026-05-10 20:10:04 +0700

    Update Trigger.cs

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -14,7 +14,7 @@ public class Trigger : MonoBehaviour
         left.SetActive(true);
         right.SetActive(true);
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         
